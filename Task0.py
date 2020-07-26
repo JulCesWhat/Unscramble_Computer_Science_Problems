c271790683da662eaddbc734ac10744f53a78142
Whatley, Cesar <julceswhat@gmail.com>
2020-07-25 20:12:58 -0400

    Adding solution for first question

--- Task0.py
+++ Task0.py
@@ -1,15 +1,22 @@
+#!/usr/bin/env python3
+
 """
 Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
 
-with open('calls.csv', 'r') as f:
-    reader = csv.reader(f)
-    calls = list(reader)
+def getCSVData():
+    texts = []
+    calls = []
+    with open('texts.csv', 'r') as f:
+        reader = csv.reader(f)
+        texts = list(reader)
+
+    with open('calls.csv', 'r') as f:
+        reader = csv.reader(f)
+        calls = list(reader)
+    return (texts, calls)
 
 
 """
@@ -20,3 +27,7 @@ Print messages:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+if __name__ == '__main__':
+    texts, calls = getCSVData()
+    print("First record of texts, {} texts {} at time {}".format(texts[0][0], texts[0][1], texts[0][2]))
+    print("Last record of calls, {} calls {} at time {}, lasting {} seconds".format(calls[0][0], calls[0][1], calls[0][2], calls[0][3]))
